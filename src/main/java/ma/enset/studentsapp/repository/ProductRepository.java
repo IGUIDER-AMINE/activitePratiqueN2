c23a75229aaeb098d261970ccde463bd97d0ad45
IGUIDER AMINE <amineiguider2017@gmail.com>
2024-03-16 16:10:34 +0100

    push

---
+++ src/main/java/ma/enset/studentsapp/repository/ProductRepository.java
@@ -0,0 +1,19 @@
+package ma.enset.studentsapp.repository;
+
+import ma.enset.studentsapp.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> { // Spring Data
+    List<Product> findByNameContains(String mc);// respect la typologie des noms
+    List<Product> findByPriceGreaterThan(double price);
+
+    @Query("select p from Product p where p.name like :x")
+    List<Product> search(@Param("x") String mc);
+
+    @Query("select p from Product p where p.price>:x")
+    List<Product> searchByPrice(@Param("x") double price);
+}
